29e8fe093efcf7db0d3ee0a13cb86efb9856ea51
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-07-02 08:45:50 +0530

    routes updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
-import Pokemons from './pages/Pokemons';
+import { Routes, Route } from 'react-router-dom';
 
+import Pokemons from './pages/Pokemons';
+import SinglePokemon from './pages/SinglePokemon';
 import './index.css';
 
 function App() {
 	return (
-		<div className="App">
-			<Pokemons />
+		<div className="min-h-screen bg-gray-200">
+			<Routes>
+				<Route path="/" element={<Pokemons />} />
+				<Route path="/pokemon/:name" element={<SinglePokemon />} />
+			</Routes>
 		</div>
 	);
 }
