13a999bae2617504df7dbef5ff49e089dba244a8
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-07-02 06:59:06 +0530

    card changes

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
 const PokeCard = ({ pokemon }) => {
-	console.log(pokemon);
 	return (
-		<div className="bg-black hover:opacity-80 container mx-auto rounded-bl-3xl rounded-tr-3xl text-white flex flex-col items-center gap-1 p-6 cursor-pointer">
+		<div
+			className={`bg-${pokemon.types[0].type.name} hover:opacity-80 container mx-auto rounded-bl-3xl rounded-tr-3xl text-white flex flex-col items-center gap-1 p-6 cursor-pointer`}
+		>
 			<div>
-				<img
-					className={`bg-${pokemon.types[0].type.name} rounded-full`}
-					src={pokemon.sprites.front_default}
-					alt="pokemon"
-				/>
+				<img src={pokemon.sprites.front_default} alt="pokemon" />
 			</div>
 			<div>
 				<h1 className="font-bold uppercase text-2xl">{pokemon.name}</h1>
@@ -21,7 +18,7 @@ const PokeCard = ({ pokemon }) => {
 			<div className="flex gap-5 pt-5">
 				{pokemon.types.map((type) => (
 					<p
-						className={` bg-${type.type.name} px-4 py-2 border-2 border-gray-400`}
+						className={` bg-${type.type.name} px-4 py-2 border-2 border-${type.type.name} rounded-lg capitalize`}
 					>
 						{type.type.name}
 					</p>
