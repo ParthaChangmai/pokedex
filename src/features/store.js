c7d9524a0755bc6383d8d19690f8c400e14e2573
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-07-02 08:46:15 +0530

    new reducer

--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit/';
 import pokemonsReducer from './slice/pokemonSlice';
+import singlePokemonReducer from './slice/singlePokemonSlice';
 
 export default configureStore({
 	reducer: {
 		pokemons: pokemonsReducer,
+		singlePokemon: singlePokemonReducer,
 	},
 });
